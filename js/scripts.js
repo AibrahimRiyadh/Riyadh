8fd9691ae536ea5a97c6f37310bdfd2a3f583347
Aibrahim Riyadh <aibrahimriyadh@gmail.com>
2020-05-26 15:44:49 +0600

    Update scripts.js
    Year

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ window.onload = function() {
     'I\'m currently accepting freelancing Work.<br> You can contact me at <a href="mailto:aibrahim@riyadh.eu.org">aibrahim@riyadh.eu.org</a>',
     '<a target="_blank" href="https://twitter.com/AibrahimRiyadh">twitter.com/AibrahimRiyadh</a><br><a target="_blank" href="https://t.me/AibrahimRiyadh">t.me/AibrahimRiyadh</a><br><a target="_blank" href="https://facebook.com/AibrahimRiyadh">facebook.com/AibrahimRiyadh</a>',
     getCurrentTime(),
-    '©2019- Aibrahim Riyadh'
+    '©2019 - 2020 Aibrahim Riyadh'
   ]
 
   var getFontSize = function() {
